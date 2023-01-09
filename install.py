237fa4492792bb61a771e9f0585071d98b9b2452
Godopu <godopu16@gmail.com>
2023-01-09 17:21:00 +0900

    add install.py

---
+++ install.py
@@ -0,0 +1,30 @@
+import os 
+import sys
+
+serviceTemplate = '''[Unit]
+Description=Run Smart Farm PoC Controller
+After=multi-user.target
+
+[Service]
+Type=simple
+WorkingDirectory={}
+ExecStart={}/controller-app
+Restart=always
+RestartSec=10s
+
+[Install]
+WantedBy=multi-user.target
+'''
+
+curDir = os.getcwd()
+service = serviceTemplate.format(curDir, curDir)
+
+with open('sfpoc.service', 'w') as f:
+    f.write(service)
+
+os.system('sudo chmod 644 sfpoc.service')
+os.system('sudo mv sfpoc.service /lib/systemd/system/')
+os.system('sudo systemctl daemon-reload')
+os.system('sudo systemctl enable sfpoc.service')
+
+
